f35782a98d1a2d1e4a98f64d113e562f2532cc09
justin <kinhanjustin@gmail.com>
2025-04-18 13:19:44 +0200

    Beeldherkenning Project added

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -13,20 +13,26 @@ function Projects() {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-6xl">
                 <div
                     className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 p-6 rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl hover:shadow-yellow-500/50 transition-transform duration-300">
-                    <h2 className="text-2xl font-bold mb-4 text-yellow-400">Project 1</h2>
-                    <img src="./projectImages/test.jpg" alt="Beschrijving van project 1" className="mb-4 rounded-lg"/>
+                    <h2 className="text-2xl font-bold mb-4 text-yellow-400">Taalmodel (Binnenkort)</h2>
+                    <img src="./projectImages/Beelherkenning.png" alt="project 1" className="mb-4 rounded-lg"/>
                     <a href="#" className="text-yellow-400 hover:text-yellow-300 hover:underline">Bekijk project</a>
                 </div>
                 <div
                     className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 p-6 rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl hover:shadow-yellow-500/50 transition-transform duration-300">
-                    <h2 className="text-2xl font-bold mb-4 text-yellow-400">Project 2</h2>
-                    <img src="/path-to-image2.jpg" alt="Beschrijving van project 2" className="mb-4 rounded-lg"/>
+                    <h2 className="text-2xl font-bold mb-4 text-yellow-400">Beeldherkenning model</h2>
+                    <img src="./projectImages/Beeldherkenning.png" alt="project 2" className="mb-4 rounded-lg"/>
                     <a href="#" className="text-yellow-400 hover:text-yellow-300 hover:underline">Bekijk project</a>
                 </div>
                 <div
                     className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 p-6 rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl hover:shadow-yellow-500/50 transition-transform duration-300">
-                    <h2 className="text-2xl font-bold mb-4 text-yellow-400">Project 2</h2>
-                    <img src="/path-to-image2.jpg" alt="Beschrijving van project 2" className="mb-4 rounded-lg"/>
+                    <h2 className="text-2xl font-bold mb-4 text-yellow-400">Hotspots (Binnenkort)</h2>
+                    <img src="/path-to-image2.jpg" alt="project 3" className="mb-4 rounded-lg"/>
+                    <a href="#" className="text-yellow-400 hover:text-yellow-300 hover:underline">Bekijk project</a>
+                </div>
+                <div
+                    className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 p-6 rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl hover:shadow-yellow-500/50 transition-transform duration-300">
+                    <h2 className="text-2xl font-bold mb-4 text-yellow-400">Nederlandse Gebarentaal</h2>
+                    <img src="/path-to-image2.jpg" alt="project 5" className="mb-4 rounded-lg"/>
                     <a href="#" className="text-yellow-400 hover:text-yellow-300 hover:underline">Bekijk project</a>
                 </div>
                 {/* Voeg meer projecten toe */}
