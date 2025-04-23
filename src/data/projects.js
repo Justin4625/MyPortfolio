aa35588bef4c7e6cbb397767cb49b0610f6ea991
justin <kinhanjustin@gmail.com>
2025-04-23 18:36:22 +0200

    Data layout change

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,116 +1,60 @@
 const projects = [
     {
         id: 1,
-        title: "Taalmodel (Binnenkort)",
-        description: "Dit project is op dit moment nog in ontwikkeling",
-        image: "./projectImages/comingSoon.jpg",
+        title: "Vakantiebestemmingen",
+        description: "Dit was mijn allereerste schoolopdracht. Het is een eenvoudige website, gebouwd met HTML en CSS, waarin je een vakantiebestemming kunt boeken.",
+        image: "./projectImages/Vakantiebestemmingen.png",
         images: [
-            "../projectImages/comingSoon.jpg"
+            "../projectImages/Vakantiebestemmingen.png",
+            "../projectImages/Vakantiebestemmingen2.png",
+            "../projectImages/Vakantiebestemmingen3.png",
         ],
-        github: "https://github.com/Justin4625/Taalmodel-Eindopdracht-02-PRG-08",
+        github: "https://github.com/Justin4625/Vakantiebestemming-FE-Eindopdracht",
+        live: "https://justin4625.github.io/Vakantiebestemming-FE-Eindopdracht/",
         languages: [
-            "../iconImages/js.png",
-            "../iconImages/react.png",
-            "../iconImages/express.png",
+            "../iconImages/HTML5.webp",
+            "../iconImages/CSS.svg",
         ]
     },
     {
         id: 2,
-        title: "Beeldherkenning model",
-        description: "Dit project was onderdeel van een schoolopdracht. Het maakt gebruik van een beeldherkenningsmodel dat is getraind op een dataset van 1000 afbeeldingen. Het model is gebaseerd op een neuraal netwerk en is in staat om objecten in afbeeldingen te identificeren en classificeren. Ik heb dit model vervolgens omgebouwd tot een soort game, waarbij gebruikers spelenderwijs interactie kunnen hebben met de beeldherkenning.",
-        image: "./projectImages/Beeldherkenning.png",
+        title: "Space Chicken",
+        description: "Dit project was onderdeel van een schoolopdracht. Space Chicken is een game waarin je als vliegende kip zoveel mogelijk muntjes moet verzamelen om een zo hoog mogelijk record te behalen, terwijl je obstakels ontwijkt.",
+        image: "./projectImages/Spacechicken.png",
         images: [
-            "../projectImages/Beeldherkenning.png",
-            "../projectImages/Beeldherkenning2.png",
-            "../projectImages/Beeldherkenning3.png"
+            "../projectImages/Spacechicken.png",
+            "../projectImages/Spacechicken2.png",
+            "../projectImages/Spacechicken3.png",
+            "../projectImages/Spacechicken4.png",
         ],
-        github: "https://github.com/Justin4625/Interface-PRG08-Eindopdracht01",
-        live: "https://justin4625.github.io/Interface-PRG08-Eindopdracht01/",
+        github: "https://github.com/Justin4625/Space-Chicken---PRG4-Eindopdracht",
+        live: "https://justin4625.github.io/Space-Chicken---PRG4-Eindopdracht/",
         languages: [
-            "../iconImages/HTML5.webp",
-            "../iconImages/CSS.svg",
-            "../iconImages/js.png",
+            "../iconImages/excalibur.png",
         ]
     },
     {
         id: 3,
-        title: "Hotspots (Binnenkort)",
-        description: "Dit project is op dit moment nog in ontwikkeling",
-        image: "./projectImages/comingSoon.jpg",
+        title: "Earthifacts",
+        description: "Dit project is in 13 dagen ontwikkeld door een team van vier. Het resultaat is Earthifacts, een RPG-game waarin je rond de Kralingse Plas op zoek gaat naar vier verborgen 'earthifacts'. Deze vind je door met NPC's te praten en de omgeving te verkennen.",
+        image: "./projectImages/Earthifacts.png",
         images: [
-            "../projectImages/comingSoon.jpg"
+            "../projectImages/Earthifacts.png",
+            "../projectImages/Earthifacts2.png",
+            "../projectImages/Earthifacts3.png",
+            "../projectImages/Earthifacts4.png",
+            "../projectImages/Earthifacts5.png",
+            "../projectImages/Earthifacts6.png",
+            "../projectImages/Earthifacts7.png",
+            "../projectImages/Earthifacts8.png",
         ],
-        github: "https://github.com/Justin4625/Hotspots-Eindopdracht-PRG07"
+        github: "https://github.com/Justin4625/Earthifacts-CLE4",
+        languages: [
+            "../iconImages/excalibur.png",
+        ]
     },
     {
         id: 4,
-        title: "Nederlandse Gebarentaal",
-        description: "Samen met een team van vijf heb ik een app ontwikkeld voor studenten in 12 dagen tijd, met als doel om het leren van Nederlandse Gebarentaal makkelijker en toegankelijker te maken. De app is ontworpen om ‘on the go’ gebruikt te worden op de telefoon, zodat studenten altijd en overal kunnen oefenen.",
-        image: "./projectImages/NederlandseGebarentaal.png",
-        images: [
-            "../projectImages/NederlandseGebarentaal.png",
-            "../projectImages/NederlandseGebarentaal2.png",
-            "../projectImages/NederlandseGebarentaal3.png",
-            "../projectImages/NederlandseGebarentaal4.png",
-            "../projectImages/NederlandseGebarentaal5.png",
-            "../projectImages/NederlandseGebarentaal6.png",
-            "../projectImages/NederlandseGebarentaal7.png",
-            "../projectImages/NederlandseGebarentaal8.png",
-            "../projectImages/NederlandseGebarentaal9.png",
-        ],
-        github: "https://github.com/hugodeman/SM-3_team-3",
-        languages: [
-            "../iconImages/react.png",
-            "../iconImages/laravel.png",
-        ]
-    },
-    {
-        id: 5,
-        title: "Open Hiring",
-        description: "The Open Hiring Project was een project dat we in een team van vijf studenten in 12 dagen hebben ontwikkeld. We kregen deze uitdaging van IO Digital, met als doel een platform te creëren voor inclusieve werving. Het idee achter Open Hiring is dat het traditionele sollicitatieproces wordt overgeslagen, waardoor kandidaten direct aan de slag kunnen.",
-        image: "./projectImages/OpenHiring.png",
-        images: [
-            "../projectImages/OpenHiring.png",
-            "../projectImages/OpenHiring2.png",
-            "../projectImages/OpenHiring3.png",
-            "../projectImages/OpenHiring4.png",
-            "../projectImages/OpenHiring5.png",
-            "../projectImages/OpenHiring6.png",
-            "../projectImages/OpenHiring7.png",
-            "../projectImages/OpenHiring8.png",
-            "../projectImages/OpenHiring9.png",
-            "../projectImages/OpenHiring10.png",
-            "../projectImages/OpenHiring11.png",
-            "../projectImages/OpenHiring12.png",
-        ],
-        github: "https://github.com/Priy4s/TLE_1_Agency",
-        languages: [
-            "../iconImages/laravel.png",
-            "../iconImages/js.png",
-            "../iconImages/CSS.svg",
-        ]
-    },
-    {
-        id: 6,
-        title: "Pokémon Compendium",
-        description: "Dit project was onderdeel van een schoolopdracht. Het is een Pokémon-compendium waarin gebruikers informatie kunnen opzoeken over verschillende Pokémon. Gebruikers kunnen Pokémon liken en zelf nieuwe Pokémon toevoegen. Toegevoegde Pokémon worden pas zichtbaar voor iedereen nadat een admin ze heeft goedgekeurd. De applicatie maakt gebruik van CRUD-functionaliteit (Create, Read, Update, Delete).",
-        image: "./projectImages/Pokemoncompendium.png",
-        images: [
-            "../projectImages/Pokemoncompendium.png",
-            "../projectImages/Pokemoncompendium2.png",
-            "../projectImages/Pokemoncompendium3.png",
-            "../projectImages/Pokemoncompendium4.png",
-            "../projectImages/Pokemoncompendium5.png",
-            "../projectImages/Pokemoncompendium6.png",
-            "../projectImages/Pokemoncompendium7.png",
-        ],
-        github: "https://github.com/Justin4625/PokemonCompendium-PRG05-Eindopracht",
-        languages: [
-            "../iconImages/laravel.png",
-        ]
-    },
-    {
-        id: 7,
         title: "Shinydex",
         description: "Dit was een persoonlijk project. Het is een Shinydex waarin je als gebruiker kunt bijhouden welke Shiny Pokémon je al hebt verzameld en hoeveel voortgang je hebt geboekt in je Pokédex.",
         image: "./projectImages/Shinydex.png",
@@ -136,57 +80,113 @@ const projects = [
         ]
     },
     {
-        id: 8,
-        title: "Earthifacts",
-        description: "Dit project is in 13 dagen ontwikkeld door een team van vier. Het resultaat is Earthifacts, een RPG-game waarin je rond de Kralingse Plas op zoek gaat naar vier verborgen 'earthifacts'. Deze vind je door met NPC's te praten en de omgeving te verkennen.",
-        image: "./projectImages/Earthifacts.png",
+        id: 5,
+        title: "Pokémon Compendium",
+        description: "Dit project was onderdeel van een schoolopdracht. Het is een Pokémon-compendium waarin gebruikers informatie kunnen opzoeken over verschillende Pokémon. Gebruikers kunnen Pokémon liken en zelf nieuwe Pokémon toevoegen. Toegevoegde Pokémon worden pas zichtbaar voor iedereen nadat een admin ze heeft goedgekeurd. De applicatie maakt gebruik van CRUD-functionaliteit (Create, Read, Update, Delete).",
+        image: "./projectImages/Pokemoncompendium.png",
         images: [
-            "../projectImages/Earthifacts.png",
-            "../projectImages/Earthifacts2.png",
-            "../projectImages/Earthifacts3.png",
-            "../projectImages/Earthifacts4.png",
-            "../projectImages/Earthifacts5.png",
-            "../projectImages/Earthifacts6.png",
-            "../projectImages/Earthifacts7.png",
-            "../projectImages/Earthifacts8.png",
+            "../projectImages/Pokemoncompendium.png",
+            "../projectImages/Pokemoncompendium2.png",
+            "../projectImages/Pokemoncompendium3.png",
+            "../projectImages/Pokemoncompendium4.png",
+            "../projectImages/Pokemoncompendium5.png",
+            "../projectImages/Pokemoncompendium6.png",
+            "../projectImages/Pokemoncompendium7.png",
         ],
-        github: "https://github.com/Justin4625/Earthifacts-CLE4",
+        github: "https://github.com/Justin4625/PokemonCompendium-PRG05-Eindopracht",
         languages: [
-            "../iconImages/excalibur.png",
+            "../iconImages/laravel.png",
         ]
     },
     {
-        id: 9,
-        title: "Space Chicken",
-        description: "Dit project was onderdeel van een schoolopdracht. Space Chicken is een game waarin je als vliegende kip zoveel mogelijk muntjes moet verzamelen om een zo hoog mogelijk record te behalen, terwijl je obstakels ontwijkt.",
-        image: "./projectImages/Spacechicken.png",
+        id: 6,
+        title: "Open Hiring",
+        description: "The Open Hiring Project was een project dat we in een team van vijf studenten in 12 dagen hebben ontwikkeld. We kregen deze uitdaging van IO Digital, met als doel een platform te creëren voor inclusieve werving. Het idee achter Open Hiring is dat het traditionele sollicitatieproces wordt overgeslagen, waardoor kandidaten direct aan de slag kunnen.",
+        image: "./projectImages/OpenHiring.png",
         images: [
-            "../projectImages/Spacechicken.png",
-            "../projectImages/Spacechicken2.png",
-            "../projectImages/Spacechicken3.png",
-            "../projectImages/Spacechicken4.png",
+            "../projectImages/OpenHiring.png",
+            "../projectImages/OpenHiring2.png",
+            "../projectImages/OpenHiring3.png",
+            "../projectImages/OpenHiring4.png",
+            "../projectImages/OpenHiring5.png",
+            "../projectImages/OpenHiring6.png",
+            "../projectImages/OpenHiring7.png",
+            "../projectImages/OpenHiring8.png",
+            "../projectImages/OpenHiring9.png",
+            "../projectImages/OpenHiring10.png",
+            "../projectImages/OpenHiring11.png",
+            "../projectImages/OpenHiring12.png",
         ],
-        github: "https://github.com/Justin4625/Space-Chicken---PRG4-Eindopdracht",
-        live: "https://justin4625.github.io/Space-Chicken---PRG4-Eindopdracht/",
+        github: "https://github.com/Priy4s/TLE_1_Agency",
         languages: [
-            "../iconImages/excalibur.png",
+            "../iconImages/laravel.png",
+            "../iconImages/js.png",
+            "../iconImages/CSS.svg",
+        ]
+    },
+    {
+        id: 7,
+        title: "Nederlandse Gebarentaal",
+        description: "Samen met een team van vijf heb ik een app ontwikkeld voor studenten in 12 dagen tijd, met als doel om het leren van Nederlandse Gebarentaal makkelijker en toegankelijker te maken. De app is ontworpen om ‘on the go’ gebruikt te worden op de telefoon, zodat studenten altijd en overal kunnen oefenen.",
+        image: "./projectImages/NederlandseGebarentaal.png",
+        images: [
+            "../projectImages/NederlandseGebarentaal.png",
+            "../projectImages/NederlandseGebarentaal2.png",
+            "../projectImages/NederlandseGebarentaal3.png",
+            "../projectImages/NederlandseGebarentaal4.png",
+            "../projectImages/NederlandseGebarentaal5.png",
+            "../projectImages/NederlandseGebarentaal6.png",
+            "../projectImages/NederlandseGebarentaal7.png",
+            "../projectImages/NederlandseGebarentaal8.png",
+            "../projectImages/NederlandseGebarentaal9.png",
+        ],
+        github: "https://github.com/hugodeman/SM-3_team-3",
+        languages: [
+            "../iconImages/react.png",
+            "../iconImages/laravel.png",
+        ]
+    },
+    {
+        id: 8,
+        title: "Hotspots (Binnenkort)",
+        description: "Dit project is op dit moment nog in ontwikkeling",
+        image: "./projectImages/comingSoon.jpg",
+        images: [
+            "../projectImages/comingSoon.jpg"
+        ],
+        github: "https://github.com/Justin4625/Hotspots-Eindopdracht-PRG07"
+    },
+    {
+        id: 9,
+        title: "Beeldherkenning model",
+        description: "Dit project was onderdeel van een schoolopdracht. Het maakt gebruik van een beeldherkenningsmodel dat is getraind op een dataset van 1000 afbeeldingen. Het model is gebaseerd op een neuraal netwerk en is in staat om objecten in afbeeldingen te identificeren en classificeren. Ik heb dit model vervolgens omgebouwd tot een soort game, waarbij gebruikers spelenderwijs interactie kunnen hebben met de beeldherkenning.",
+        image: "./projectImages/Beeldherkenning.png",
+        images: [
+            "../projectImages/Beeldherkenning.png",
+            "../projectImages/Beeldherkenning2.png",
+            "../projectImages/Beeldherkenning3.png"
+        ],
+        github: "https://github.com/Justin4625/Interface-PRG08-Eindopdracht01",
+        live: "https://justin4625.github.io/Interface-PRG08-Eindopdracht01/",
+        languages: [
+            "../iconImages/HTML5.webp",
+            "../iconImages/CSS.svg",
+            "../iconImages/js.png",
         ]
     },
     {
         id: 10,
-        title: "Vakantiebestemmingen",
-        description: "Dit was mijn allereerste schoolopdracht. Het is een eenvoudige website, gebouwd met HTML en CSS, waarin je een vakantiebestemming kunt boeken.",
-        image: "./projectImages/Vakantiebestemmingen.png",
+        title: "Taalmodel (Binnenkort)",
+        description: "Dit project is op dit moment nog in ontwikkeling",
+        image: "./projectImages/comingSoon.jpg",
         images: [
-            "../projectImages/Vakantiebestemmingen.png",
-            "../projectImages/Vakantiebestemmingen2.png",
-            "../projectImages/Vakantiebestemmingen3.png",
+            "../projectImages/comingSoon.jpg"
         ],
-        github: "https://github.com/Justin4625/Vakantiebestemming-FE-Eindopdracht",
-        live: "https://justin4625.github.io/Vakantiebestemming-FE-Eindopdracht/",
+        github: "https://github.com/Justin4625/Taalmodel-Eindopdracht-02-PRG-08",
         languages: [
-            "../iconImages/HTML5.webp",
-            "../iconImages/CSS.svg",
+            "../iconImages/js.png",
+            "../iconImages/react.png",
+            "../iconImages/express.png",
         ]
     },
 ];
